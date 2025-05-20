396bd0085c500399876eba5cbdf6fe8bf0e4330a
deviled <gudeliunas.mindaugas@gmail.com>
2025-05-20 22:44:09 +0300

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -27,6 +28,7 @@ export default function RootLayout({
           <Toaster richColors theme="system" />
         </main>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
